7a45226f7b177809e1ac2d05b06f13d4d6b48ee6
BananaBoii <98301106+BananaBoii600@users.noreply.github.com>
2022-03-26 11:43:47 +0530

    Main Files

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+
+with open("wordlist.txt", "r") as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+
+allowed_errors = 7
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+    guess = input(f"Allowed Mistakes Left {allowed_errors}, Next Guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+
+if done:
+    print(f"You found the word! It was {word}")
+else:
+    print(f"Game Over! The word was {word}")
+
+input("Enter any key to quit")
